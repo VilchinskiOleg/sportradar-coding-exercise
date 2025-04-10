9245b2b66e502cbd37f1f2f333bf3aedd24c1cac
aleh.vilchynski <vilchinskioleg@gmail.com>
2025-04-10 10:29:13 +0200

    Implement WorldCupScoreboardService

--- src/main/java/org/sportradar/lib/service/WorldCupScoreboardService.java
+++ src/main/java/org/sportradar/lib/service/WorldCupScoreboardService.java
@@ -2,9 +2,12 @@ package org.sportradar.lib.service;
 
 import lombok.RequiredArgsConstructor;
 import org.sportradar.lib.model.Match;
+import org.sportradar.lib.model.Team;
 import org.sportradar.lib.service.dao.MatchScoreboardDao;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class WorldCupScoreboardService {
@@ -12,18 +15,27 @@ public class WorldCupScoreboardService {
     private final MatchScoreboardDao matchScoreboardDao;
 
     public Collection<Match> initiateNewMatch(String homeTeamName, String awayTeamName) {
-        return null;
+        var newMatch = new Match(
+                UUID.randomUUID().toString(),
+                LocalDateTime.now(),
+                initiateTeam(homeTeamName),
+                initiateTeam(awayTeamName));
+        return matchScoreboardDao.create(newMatch);
     }
 
     public Collection<Match> getAllMatches() {
-        return null;
+        return matchScoreboardDao.findAll();
     }
 
     public Collection<Match> updateScore(String matchId, int homeTeamScore, int awayTeamScore) {
-        return null;
+        return matchScoreboardDao.updateScore(matchId, homeTeamScore, awayTeamScore);
     }
 
     public Collection<Match> deleteMatch(String matchId) {
-        return null;
+        return matchScoreboardDao.delete(matchId);
+    }
+
+    private Team initiateTeam(String name) {
+        return new Team(UUID.randomUUID().toString(), name, 0);
     }
 }
